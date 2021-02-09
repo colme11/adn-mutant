87de7f0b2861b75509a61639d272cb33db5c3eb0
colme11 <colme_11@hotmail.com>
2021-02-08 22:49:33 -0500

    :sparkles:    initial code

---
+++ src/main/java/com/mercadolibre/adnmutant/AdnMutantApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.adnmutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnMutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnMutantApplication.class, args);
+	}
+
+}
